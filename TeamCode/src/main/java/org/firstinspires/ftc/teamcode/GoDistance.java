d29e1db83964ff79872ab7e0ec86d48c40a210dc
DESKTOP-4ABI2LU\SREYASHKUNAAL <kunaalsrini@gmail.com>
2020-10-03 21:20:38 -0400

    New GoDistance Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoDistance.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class GoDistance extends LinearOpMode {
+
+    static final int MOTOR_TICKS_COUNTS = 1120;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor backRightMotor = null;
+    public void GoDistanceCM(int centimeters, double power){
+
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        double conversion = 3.14/360;
+        double encoders = centimeters*conversion;
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(power);
+        backLeftMotor.setPower(power);
+        backRightMotor.setPower(power);
+
+        while(opModeIsActive() && Math.abs(frontLeftMotor.getCurrentPosition()) < encoders && Math.abs(frontRightMotor.getCurrentPosition()) < encoders && Math.abs(backLeftMotor.getCurrentPosition()) < encoders && Math.abs(backRightMotor.getCurrentPosition()) < encoders) {
+
+        }
+
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+
+
+
+
+    }
+    @Override
+    public void runOpMode() {
+
+        GoDistanceCM(25, 0.5);
+
+    }
+}
