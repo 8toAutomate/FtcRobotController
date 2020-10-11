6593ad8698afdd573ccf5efc9525e814f327681a
LAPTOP-AA6EBAJ7\rrall <Sathvik.allipuram@gmail.com>
2020-10-10 21:46:04 -0400

    I created the ring finder program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/Ring_Finder.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Sensors;
+
+public class Rind_FInder {
+}
+    int RingsFound = 0;
+    if (colorSensor instanceof DistanceSensor) {
+        telemetry.addData("Distance (cm)", "%.3f", ((DistanceSensor) colorSensor).getDistance(DistanceUnit.CM));
+    }
+    window.print
+    
