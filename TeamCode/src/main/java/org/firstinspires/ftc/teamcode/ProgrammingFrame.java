cafcf613ba66bb9b9cf7d92520550168deb8bba3
DESKTOP-4ABI2LU\SREYASHKUNAAL <kunaalsrini@gmail.com>
2020-11-14 20:40:58 -0500

    Added StrafeDistance function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingFrame.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingFrame.java
@@ -55,6 +55,8 @@ public class ProgrammingFrame
     public DcMotor backLeftMotor = null;
     public DcMotor backRightMotor = null;
 
+    public DistanceSensor sensorRange;
+
     public NormalizedColorSensor colorSensor1;
     public NormalizedColorSensor colorSensor2;
 
@@ -100,6 +102,7 @@ public class ProgrammingFrame
 //        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Define and initialize ALL installed servos.
+        sensorRange = hwMap.get(DistanceSensor.class, "sensor_range");
         colorSensor1 = hwMap.get(NormalizedColorSensor.class, "sensor_color");
         colorSensor2 = hwMap.get(NormalizedColorSensor.class, "sensor_color");
 
@@ -431,6 +434,10 @@ public class ProgrammingFrame
         systemTools.telemetry.addData("Path letter (E is Error): ", path);
         systemTools.telemetry.update();
         return path;
+    }
+
+    public double getDistanceCM() {
+        return sensorRange.getDistance(DistanceUnit.CM);
     }
 
     public void stopDriveMotors() {
