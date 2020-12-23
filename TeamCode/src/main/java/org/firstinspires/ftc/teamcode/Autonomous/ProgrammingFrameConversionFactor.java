a3e97e17f31acb1168d063639d3d2075e07308a7
Thanish <js2t@outlook.com>
2020-12-22 20:33:11 -0500

    updated conversion factor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ProgrammingFrameConversionFactor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ProgrammingFrameConversionFactor.java
@@ -77,24 +77,14 @@ public class ProgrammingFrameConversionFactor extends LinearOpMode {
     static final double     timeoutS                = 20;
 
 
+    public void GoDistanceTICKS(int ticks, double power) {
 
-
-    public void runOpMode() {
-
-        /*
-         * Initialize the drive system variables.
-         * The init() method of the hardware class does all the work here
-         */
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.addData("Status", "Resetting Encoders");
         telemetry.update();
 
-        robot.frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+        robot.resetDriveEncoders();
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Path0", "Starting at %7d :%7d",
@@ -103,13 +93,11 @@ public class ProgrammingFrameConversionFactor extends LinearOpMode {
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
-        robot.init(hardwareMap, this);
-        waitForStart();
 
         int FLtarget = robot.frontLeftMotor.getCurrentPosition() + TICKS;
-        int FRtarget = robot.frontLeftMotor.getCurrentPosition() + TICKS;
-        int BLtarget = robot.frontLeftMotor.getCurrentPosition() + TICKS;
-        int BRtarget = robot.frontLeftMotor.getCurrentPosition() + TICKS;
+        int FRtarget = robot.frontRightMotor.getCurrentPosition() + TICKS;
+        int BLtarget = robot.backLeftMotor.getCurrentPosition() + TICKS;
+        int BRtarget = robot.backRightMotor.getCurrentPosition() + TICKS;
 
         robot.frontLeftMotor.setTargetPosition(FLtarget);
         robot.frontRightMotor.setTargetPosition(FRtarget);
@@ -123,10 +111,10 @@ public class ProgrammingFrameConversionFactor extends LinearOpMode {
 
         // reset the timeout time and start motion.
         runtime.reset();
-        robot.frontLeftMotor.setPower(DRIVE_SPEED);
-        robot.frontRightMotor.setPower(DRIVE_SPEED);
-        robot.backRightMotor.setPower(DRIVE_SPEED);
-        robot.backLeftMotor.setPower(DRIVE_SPEED);
+        robot.frontLeftMotor.setPower(power);
+        robot.frontRightMotor.setPower(power);
+        robot.backRightMotor.setPower(power);
+        robot.backLeftMotor.setPower(power);
 
         // keep looping while we are still active, and there is time left, and both motors are running.
         // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
@@ -135,25 +123,22 @@ public class ProgrammingFrameConversionFactor extends LinearOpMode {
         // However, if you require that BOTH motors have finished their moves before the robot continues
         // onto the next step, use (isBusy() || isBusy()) in the loop test.
         while (opModeIsActive() &&
-                (runtime.seconds() < timeoutS) &&
+                (runtime.seconds() < 30) &&
                 (Math.abs(robot.frontLeftMotor.getCurrentPosition()) < TICKS && Math.abs(robot.frontRightMotor.getCurrentPosition()) < TICKS && Math.abs(robot.backLeftMotor.getCurrentPosition()) < TICKS && Math.abs(robot.backRightMotor.getCurrentPosition()) < TICKS)) {
         }
 
-        robot.frontLeftMotor.setPower(0);
-        robot.frontRightMotor.setPower(0);
-        robot.backRightMotor.setPower(0);
-        robot.backLeftMotor.setPower(0);
+        robot.stopDriveMotors();
 
-        robot.frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.startDriveEncoders();
 
         telemetry.addData("Path", "Complete");
         telemetry.addData("counts", TICKS);
         telemetry.update();
+    }
 
 
 
+    public void runOpMode() {
+        GoDistanceTICKS(100, 0.8);
     }
 }
