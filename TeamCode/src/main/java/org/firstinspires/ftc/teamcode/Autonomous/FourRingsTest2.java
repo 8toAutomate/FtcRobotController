e54a8b484b845b03abea861a39acab4c86f7314d
LAPTOP-F84KJ4AQ\Eighttoautomate <eighttoautomate@outlook.com>
2021-03-07 16:23:20 -0500

    Created two test programs for collecting the starter stack with 4 rings 2.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FourRingsTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FourRingsTest2.java
@@ -20,7 +20,7 @@ public class FourRingsTest2 extends LinearOpMode {
         robot.init(hardwareMap, this);
         waitForStart();
         robot.storageServo.setPosition(0);
-        robot.shooting.setPower(.78);
+        robot.shooting.setPower(.75);
         robot.goDistanceAcceleration(65,0.8,false,5,50,this);
 
         for (int i = 1; i<=3; i++) {
@@ -29,23 +29,37 @@ public class FourRingsTest2 extends LinearOpMode {
             robot.wait(400, this); // timeout was 1200 fir first tourney
         }
         robot.storageServo.setPosition(1);
-        robot.strafeDistanceCM2(23, 0.3, false, this);
+        robot.strafeDistanceCM2(23, 0.2, false, this);
         robot.intake(ProgrammingFrame.States.Forwards);
-        robot.GoDistanceCM2(17, 0.3, false, this);
+        robot.GoDistanceCM2(20, 0.3, false, this);
         sleep(2000L);
         robot.intake(ProgrammingFrame.States.Off);
         robot.shooting.setPower(.8);
         robot.storageServo.setPosition(0);
-        robot.strafeDistanceCM2(-30, .3, false, this);
+        robot.strafeDistanceCM2(-30, .2, false, this);
 
         for (int i = 1; i<=2; i++) {
             robot.pushRing(0.4, this);
             robot.flywheel(true, 0.8); // increase power for second and third shot
             robot.wait(400, this); // timeout was 1200 fir first tourney
         }
-
-
-
+        robot.storageServo.setPosition(1);
+        robot.strafeDistanceCM2(30, .2, false, this);
+        robot.intake(ProgrammingFrame.States.Forwards);
+        robot.GoDistanceCM2(35, 0.3, false, this);
+        //sleep(2000L);
+        //robot.intake(ProgrammingFrame.States.Off);
+        robot.shooting.setPower(.8);
+        //robot.storageServo.setPosition(0);
+        robot.strafeDistanceCM2(-30, .2, false, this);
+        robot.storageServo.setPosition(0);
+        robot.intake(ProgrammingFrame.States.Off);
+        sleep (300);
+        for (int i = 1; i<=3; i++) {
+            robot.pushRing(0.4, this);
+            robot.flywheel(true, 0.8); // increase power for second and third shot
+            robot.wait(400, this); // timeout was 1200 fir first tourney
+        }
 
 
     }
